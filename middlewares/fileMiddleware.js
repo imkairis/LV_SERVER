dc3e27a7e63e72d6b7b0eed01d591116851fb1c4
QuocViet <vietnguyen142002@gmail.com>
2024-10-27 22:41:00 +0700

    add check exists upload folder

--- middlewares/fileMiddleware.js
+++ middlewares/fileMiddleware.js
@@ -1,26 +1,31 @@
-const multer = require('multer');
-const path = require('path');
-
+const multer = require("multer");
+const path = require("path");
+const fsPromises = require("fs/promises");
+const fs = require("fs");
 // Configure storage for multer
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, './uploads');
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}${path.extname(file.originalname)}`);
-  }
+    destination: async (req, file, cb) => {
+        if (!fs.existsSync("./uploads")) {
+            await fsPromises.mkdir("./uploads");
+        }
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+    },
 });
 
-const uploadSingle = (fieldName) => multer({ storage }).single(fieldName);
+const uploadSingle = fieldName => multer({ storage }).single(fieldName);
 
-const uploadMultiple = (fieldName, length=10) => multer({ storage }).array(fieldName, length); 
+const uploadMultiple = (fieldName, length = 10) =>
+    multer({ storage }).array(fieldName, length);
 
-const uploadFields = (fieldsArray) => {
-  return multer({ storage }).fields(fieldsArray);
+const uploadFields = fieldsArray => {
+    return multer({ storage }).fields(fieldsArray);
 };
 
 module.exports = {
-  uploadSingle,
-  uploadMultiple,
-  uploadFields,
+    uploadSingle,
+    uploadMultiple,
+    uploadFields,
 };
